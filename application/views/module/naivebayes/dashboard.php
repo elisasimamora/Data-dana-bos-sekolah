dad9e37ec237f737d5e8081891409649ff3c6c90
triutami09 <kisarandilla98@gmail.com>
2024-01-24 17:45:25 +0700

    ci4

---
+++ application/views/module/naivebayes/dashboard.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-box" align="center">
+          <h3>Sistem Pendukung Keputusan Menggunakan NAIVE BAYES</h3>
+        </div>
+    </div>
+</div>
